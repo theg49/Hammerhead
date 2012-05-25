43bb884478d88f3e1df381fc092bf43e4bad200b
Jerry Barnes <jerryb49@gmail.com>
2012-05-24 23:39:39 -0400

    First example code for activity invoice engine

---
+++ InvoicingEngine/src/com/nj/invoicing/activity/engine/ActivityInvoiceEngine.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nj.invoicing.activity.engine;
+
+import com.nj.invoicing.interfaces.*;
+
+/**
+ *
+ * @author TheG49
+ */
+public class ActivityInvoiceEngine 
+{
+    // TODO: TheG49 - Need return class and parameter class
+    private void calculateDomesticSegmentFee(Program program)
+    {
+        
+    }
+}
---
+++ InvoicingInterfaces/src/com/nj/invoicing/interfaces/Program.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nj.invoicing.interfaces;
+
+/**
+ *
+ * @author TheG49
+ */
+public interface Program 
+{
+    public Double getDomesticSegmentFee();
+}
